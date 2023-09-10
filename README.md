b597cd5688631e76b83cb42d2f528c007bf0e75e
a113n <15912502+iosecurityio@users.noreply.github.com>
2023-09-10 06:30:29 -0400

    refactor for hostname vs subdomain

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# make_steam_link.py
+# make_steam_hyperlink.py
 
-Steam will automatically create a hyperlink in a chat message if it detects a valid URI scheme. This has gotten a little lenient and now doesn't even require defining a protocol like `https://`. This scheme is just 2 characters: `ab`, a period: `.` , and 2 more characters: `ab` (i.e. a small hostname and a small Top Level Domain) to equal `ab.ab`. That's it. These are also inherently given an `http://` protocol, but that's a different story.
+[->](ma.ke_ste.am_hyperli.nk.py) [ma.ke_ste.am_hyperli.nk.py](ma.ke_ste.am_hyperli.nk.py) [<-](ma.ke_ste.am_hyperli.nk.py)
+
+Steam will automatically create a hyperlink in a chat message if it detects a valid URI scheme. This has gotten a little lenient and now doesn't even require defining a scheme or protocol like `https://`. A Fully Qualified Domain Name in a Steam chat message is just 2 characters: `ab`, a period: `.` , and 2 more characters: `ab` (i.e. a small hostname and a small Top Level Domain) to equal `ab.ab`. That's it. These are also inherently given an `http://` protocol, but that's a different story. 📚
 
 This script creates hyperlinks that you can copy and paste into Steam chat messages for testing purposes. Change the configuration settings at the top of the script for testing, and just run it. Copy and Paste the output in your Steam chats and enjoy all of the things to click on. 🔗 
 
@@ -48,6 +50,7 @@ and finally, **linkified** text will turn every word that is >= 4 characters lon
 [*] Linkified message:
 
 Th.is messa.ge wi.ll ha.ve be.en linkifi.ed
+Plea.se downlo.ad th.is co.ol li.nk than.ks mu.ch
 ```
 
 ## Observations
@@ -88,28 +91,32 @@ There are a few constraints that I've found, some are still being tested:
 
 - The Subdomain can not be longer than 63 characters long
 
-  - I have notes of 126 maximum characters, but I can't seem to reproduce that. That's exactly 2x.
+- I have notes of 126 maximum characters, but I can't seem to reproduce that. That's exactly 2x.
 
 - The amount of subdomains is limited to 31
 
-  - I also have notes of only being allowed 15 subdomains
+- I also have notes of only being allowed 15 subdomains
 
-  - The maximum amount of subdomains possible on the internet is capped at 500 so Steam doesn't come close to that.
+- The maximum amount of subdomains possible on the internet is capped at 500 so Steam doesn't come close to that.
 
 - The Top Level Domain has to be valid, or on an allow list of sorts.
 
 - Steams character count for a message is capped at **5000 characters**.
 
+Do they differentiate domain length from subdomain? At what point does it halve the subdomains allowed? Further research required.
+
 ### Updates, Notes, and TODOs
 
-- Update: You can now Linkify your Steam messages. Example output above.
+```diff
++ Updated: You can now Linkify your Steam messages. Example output above.
 
-- NOTE: The constraints need to be ironed out more. Do they differentiate domain from subdomain? Lengths? etc.
++ Updated: Cleaned up vernacular around subdomain/domain/hostname.
 
-- TODO: Add more tests and more research into what I'm doing with my life.
+TODO: Add more tests and more research into what I'm doing with my life.
 
-- TODO: Clean up vernacular around subdomain/domain/hostname.
+TODO: Refactor into a class and add more functionality.
 
-- TODO: Refactor into a class and add more functionality.
+TODO: Make CLI version/argparse
 
 - TODO: Make friends mad.
+```
--- make_steam_link.py
+++ make_steam_link.py
@@ -1,112 +1,124 @@
 """
-Name: make_steam_link.py
+Name: make_steam_hyperlink.py
 Author: Allen Montgomery, IO Security
-Version: 0.0.2
+Version: 0.0.3
 Purpose: Creates hyperlinks for use in Steam chat messages.
 """
 
 import sys
 from colorama import Fore
 
-# Configure your URL to generate here
-SUBDOMAIN = "oo."
-DOMAIN = "ok"
+# Configure your URI to generate here
+# Change Host, Subdomains and Domain (TLD) to make a FQDN
+HOST = "i1li1li1l."
+SUBDOMAIN = "li1li1li1."
 SUBDOMAIN_COUNT = 1
+# Top Level Domain
+DOMAIN = "il"
 
-# Configure your message to linkify here:
-# If you want to turn a Steam message into a hyperlink mess, this is your place.
-MSG_TO_LINKIFY = "This message will have been linkified"
-# You can just run the script now from here, unless you want to test constraints
+# Enter a string to transform into a 'linkified' Steam chat message
+# TODO: Handle special characters :)
+MSG_TO_LINKIFY = "Please download this cool link thanks much"
 
-# Constraints
+# You can just run the script now from here, unless you want to test constraints below
+
+# CONSTRAINTS
 STEAM_MAX_SUBDOMAIN_NAME_CHARS = 63
-# The max amount of subdomains seems to vary depending on the length of the subdomain
-# All of the below have been witnessed
-# STEAM_MAX_SUBDOMAINS = 15
-# STEAM_MAX_SUBDOMAINS = 126
-# Regardless, these numbers are off, and right now you actually have 30 subdomains and a domain
-# We will call it 31 until a refactor
+# TODO: Find out how these numbers are doubled/halved in Steam messages. Interesting coincidence.
+# STEAM_MAX_SUBDOMAINS = [15, 31, 126]
 STEAM_MAX_SUBDOMAINS = 31
 STEAM_MAX_MESSAGE_CHARS = 5000
-# The overall maximum amount of subdomains allowed in a URL
-MAXIMUM_SUBDOMAINS = 500
 
 
 def make_steam_hyperlink(
-    subdomain=SUBDOMAIN, subdomain_count=SUBDOMAIN_COUNT, domain=DOMAIN, truncate=False
+    host=HOST,
+    subdomain=SUBDOMAIN,
+    subdomain_count=SUBDOMAIN_COUNT,
+    domain=DOMAIN,
+    truncate=False,
 ):
-    """Makes a steam hyperlink to be inserted into a steam message"""
+    """Makes a steam hyperlink to be inserted into a Steam chat message"""
 
+    # Handle if you dont pass in any host or subdomain
+    if host == "" and subdomain == "":
+        print("No host or subdomain specified. Exiting.")
+        sys.exit(1)
+
+    # If you dont have a host but you defined a subdomain, use the subdomain as the host
+    if host == "" and subdomain != "":
+        host = subdomain
+        subdomain_count += 1
+
+    # If you don't have a count, just return the domain name
     if subdomain_count == 0:
-        message = f"{domain}"
-        return message
+        return f"{host}{domain}"
+
+    # Check the quantity of subdomains against max limit
     if subdomain_count > STEAM_MAX_SUBDOMAINS:
         print("Too many subdomains.")
         print(
             f"Subdomain count: {Fore.RED}{subdomain_count}{Fore.RESET} / {STEAM_MAX_SUBDOMAINS}"
         )
-        sys.exit()
+        sys.exit(1)
+
+    # Check the amount of characters in your subdomain against max limit
     overage = len(subdomain) - 1
     if overage > STEAM_MAX_SUBDOMAIN_NAME_CHARS:
         print(f"Subdomain: '{subdomain}' is too long.")
         print(
             f"Characters: {Fore.RED}{overage}{Fore.RESET} / {STEAM_MAX_SUBDOMAIN_NAME_CHARS}"
         )
-        sys.exit()
+        sys.exit(1)
 
-    # Make a message
-    message = f"{subdomain_count * subdomain}{domain}"
-    # print(f"Message: \n{message}")
+    # Put together the URI for a hyperlink
+    hyperlink = f"{subdomain_count * subdomain}{host}{domain}"
 
-    # Check the length of the message
-    message_length = len(message)
+    # Check the overall length of the message
+    message_length = len(hyperlink)
+    # Choose between killing the program or truncating the message
     if message_length > STEAM_MAX_MESSAGE_CHARS:
+        # If would set truncate to True, truncate the message
         if truncate:
-            message = truncate_message(message, top_level_domain=domain)
+            message = truncate_message(message, tld=domain)
         else:
             print(f"Message is too long. Max is {STEAM_MAX_MESSAGE_CHARS}")
-            print(f"Length: {len(message)}")
-            sys.exit()
+            print(f"Length: {len(hyperlink)}")
+            sys.exit(1)
 
-    if subdomain_count > 1:
-        print(f"Subdomain: {Fore.GREEN}{subdomain}{Fore.RESET}")
-        print(
-            f"Subdomain count: {Fore.GREEN}{len(message.split('.'))-2}{Fore.RESET} / {STEAM_MAX_SUBDOMAINS}"
-        )
-        print(f"Domains: {Fore.GREEN}1{Fore.RESET}")
-    else:
-        print(f"Domain: {Fore.GREEN}{subdomain}{Fore.RESET}")
-
+    print(f"Host: {Fore.GREEN}{host}{Fore.RESET}")
+    print(f"Subdomain: {Fore.GREEN}{subdomain}{Fore.RESET}")
+    # We are subtracting 2 here for both the top level domain and the hostname
+    print(
+        f"Subdomain count: {Fore.GREEN}{len(hyperlink.split('.'))-2}{Fore.RESET} / {STEAM_MAX_SUBDOMAINS} and {Fore.GREEN}1{Fore.RESET} domain"
+    )
     print(f"Top Level Domain: {Fore.GREEN}{domain}{Fore.RESET}")
     print(
-        f"Total Message Length: {Fore.GREEN}{len(message)}{Fore.RESET} / {STEAM_MAX_MESSAGE_CHARS}"
+        f"Total Message Length: {Fore.GREEN}{len(hyperlink)}{Fore.RESET} / {STEAM_MAX_MESSAGE_CHARS}"
     )
 
-    return message
+    return hyperlink
 
 
-def truncate_message(
-    message, max_chars=STEAM_MAX_MESSAGE_CHARS, top_level_domain=DOMAIN
-):
+def truncate_message(message, max_chars=STEAM_MAX_MESSAGE_CHARS, tld=DOMAIN):
     """Truncates a message to 5000 characters and adds a top level domain"""
 
     if len(message) > max_chars:
         print("Message is too long but will be truncated.")
         print(f"Message length: {len(message)} will be cut down to {max_chars}")
     # Truncate enough to add the top level domain without going over 5000
-    message = message[: max_chars - (len(top_level_domain) + 1)]
+    message = message[: max_chars - (len(tld) + 1)]
     # check if the last character is a period
     if message[-1] == ".":
-        message = f"{message}{top_level_domain}"
+        message = f"{message}{tld}"
     else:
         # add the top level domain
-        message = f"{message}.{top_level_domain}"
+        message = f"{message}.{tld}"
 
     print("[!] Truncated message.)")
     return message
 
 
-def linkify(message=MSG_TO_LINKIFY):
+def linkify(message=MSG_TO_LINKIFY, truncate=False):
     """Converts a string of text, a message, into a hyperlinked version"""
 
     # Split the message into words
@@ -126,18 +138,32 @@ def linkify(message=MSG_TO_LINKIFY):
 
     # Join the words back together
     linkified_message = " ".join(linkified_words)
+    # Check the length of the message
+    if len(linkified_message) > STEAM_MAX_MESSAGE_CHARS:
+        if truncate:
+            linkified_message = linkified_message[:STEAM_MAX_MESSAGE_CHARS]
+        else:
+            print("Message is too long. Max is 5000")
+            print(f"Length: {len(linkified_message)}")
+            sys.exit(1)
+
     return linkified_message
 
 
 if __name__ == "__main__":
+    print(
+        f"{Fore.LIGHTGREEN_EX}->{Fore.RESET} {Fore.LIGHTBLUE_EX}ma.ke_ste.am_hyperli.nk.py{Fore.RESET} {Fore.LIGHTGREEN_EX}<-{Fore.RESET}"
+    )
+    print(f"{Fore.LIGHTGREEN_EX}Version: {Fore.RESET}0.0.3")
+    print(f"{Fore.LIGHTGREEN_EX}WHY? {Fore.RESET}No reason. Just for the lulz.")
+    print()
     # Make the hyperlink
     steam_message = make_steam_hyperlink()
 
     # Print the hyperlink
-    print(
-        f"{Fore.CYAN}\n [*] Copy the Steam hyperlink below:{Fore.RESET}")
+    print(f"{Fore.CYAN}\n [*] Copy the Steam hyperlink below:{Fore.RESET}")
     print(f"\n{steam_message}\n")
 
     # Print a linkified message
-    print(f"{Fore.MAGENTA}\n [*] Linkified message:{Fore.RESET}")
+    print(f"{Fore.MAGENTA}\n [*] Copy the Steam Linkified message:{Fore.RESET}")
     print(f"\n{linkify()}\n")
